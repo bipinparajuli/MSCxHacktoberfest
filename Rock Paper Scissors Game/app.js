2b30f655f7cc1a860e135e0e5f55041689797cd5
bipinparajuli <bipinprjl@gmail.com>
2021-10-03 19:00:25 +0545

    improved messege

--- Rock Paper Scissors Game/app.js
+++ Rock Paper Scissors Game/app.js
@@ -4,6 +4,7 @@ const userScore_span=document.getElementById("user-score");
 const computerScore_span=document.getElementById("computer-score");
 const scoreBoard_div=document.querySelector(".score-board");
 const result_p=document.querySelector(".result > p");
+const result_user=document.querySelector(".player");
 const rock_div=document.getElementById("r");
 const paper_div=document.getElementById("p");
 const scissors_div=document.getElementById("s");
@@ -20,6 +21,9 @@ function convertToWord(letter){
     return "Scissors";
 }
 
+
+
+
 function win(userChoice, computerChoice){
     const smallUserWord = "user".fontsize(3).sub();
     const smallCompWord = "comp".fontsize(3).sub();
@@ -27,9 +31,13 @@ function win(userChoice, computerChoice){
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord} You win!!`;
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}`;
+    result_user.innerHTML="You win 😃😃!!"
+    result_p.append(result_user)
     userChoice_div.classList.add('green-glow');
     setTimeout(() => userChoice_div.classList.remove('green-glow'), 300);
+    result_user.classList.add('wintext');
+    setTimeout(() => result_user.classList.remove('wintext'), 300);
 }
 function lose(userChoice, computerChoice){
     const smallUserWord = "user".fontsize(3).sub();
@@ -38,17 +46,25 @@ function lose(userChoice, computerChoice){
     computerScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord} You lost...`;
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord}`;
+    result_user.innerHTML="You lose 😞😞 . . ."
+    result_p.append(result_user)
     userChoice_div.classList.add('red-glow');
     setTimeout(() => userChoice_div.classList.remove('red-glow'), 300);
+    result_user.classList.add('losetext');
+    setTimeout(() => result_user.classList.remove('losetext'), 300);
 }
 function draw(userChoice, computerChoice){
     const smallUserWord = "user".fontsize(3).sub();
     const smallCompWord = "comp".fontsize(3).sub();
     const userChoice_div = document.getElementById(userChoice);
-    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(computerChoice)}${smallCompWord} It's a Draw.`;
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(computerChoice)}${smallCompWord}`;
+    result_user.innerHTML=" It's a draw 🙅‍♀️🙅‍♀️"
+    result_p.append(result_user)
     userChoice_div.classList.add('gray-glow');
     setTimeout(() => userChoice_div.classList.remove('gray-glow'), 300);
+    result_user.classList.add('drawtext');
+    setTimeout(() => result_user.classList.remove('drawtext'), 300);
 }
 
 function game(userChoice){
